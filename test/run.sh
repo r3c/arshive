650c52fbe26a8e7b0b84bc09726587f444840150
Remi Caput <v.mirari.fr+git@mirari.fr>
2022-04-26 23:49:01 +0200

    Run tests from GitHub actions.

--- test/run.sh
+++ test/run.sh
@@ -27,74 +27,110 @@ invoke() {
     local config="$1"
     local script="$basedir/../src/arshive.sh"
 
-    "$script" -c "$config" -d
+    "$shell" "$script" -c "$config" -d
 }
 
-basedir="$(dirname "$0")"
-worktree="$(mktemp -d)"
-config="$worktree/config"
-result=0
-stderr="$worktree/stderr"
+run() {
+    local worktree="$(mktemp -d)"
+    local config="$worktree/config"
+    local result=0
+    local shell="$1"
+    local stderr="$worktree/stderr"
 
-# Should read rule files from absolute path
-absolute="$(mktemp -d)"
+    # Should read rule files from absolute path
+    absolute="$(mktemp -d)"
 
-cat > "$config" << EOF
+    cat > "$config" << EOF
 sources='$absolute/*'
 EOF
 
-cat > "$absolute/1" << EOF
+    cat > "$absolute/1" << EOF
 test-directory1: echo -n > {}
 EOF
 
-cat > "$absolute/2" << EOF
+    cat > "$absolute/2" << EOF
 test-directory2: echo -n > {}
 EOF
 
-invoke "$config" 2> "$stderr"
-rm -r "$absolute"
+    invoke "$config" 2> "$stderr"
+    rm -r "$absolute"
 
-if ! printf '^test-directory1: should backup\n^test-directory2: should backup\n^$\n' | compare 'test-absolute' "$stderr"; then
-    result=1
-fi
+    if ! printf '^test-directory1: should backup\n^test-directory2: should backup\n^$\n' | compare 'test-absolute' "$stderr"; then
+        result=1
+    fi
 
-# Should read rule files from relative path
-relative1="$(dirname "$config")/test-relative1.rule"
-relative2="$(dirname "$config")/test-relative2.rule"
+    # Should read rule files from relative path
+    relative1="$(dirname "$config")/test-relative1.rule"
+    relative2="$(dirname "$config")/test-relative2.rule"
 
-cat > "$config" << EOF
+    cat > "$config" << EOF
 sources=test-*.rule
 EOF
 
-cat > "$relative1" << EOF
+    cat > "$relative1" << EOF
 test-relative1: echo -n > {}
 EOF
 
-cat > "$relative2" << EOF
+    cat > "$relative2" << EOF
 test-relative2: echo -n > {}
 EOF
 
-invoke "$config" 2> "$stderr"
-rm "$relative1" "$relative2"
+    invoke "$config" 2> "$stderr"
+    rm "$relative1" "$relative2"
 
-if ! printf '^test-relative1: should backup\n^test-relative2: should backup\n^$\n' | compare 'test-relative' "$stderr"; then
-    result=1
-fi
+    if ! printf '^test-relative1: should backup\n^test-relative2: should backup\n^$\n' | compare 'test-relative' "$stderr"; then
+        result=1
+    fi
 
-# Should parse and execute various rule files
-for rule in "$basedir"/*.rule; do
-    cat > "$config" << EOF
+    # Should parse and execute various rule files
+    for rule in "$basedir"/*.rule; do
+        cat > "$config" << EOF
 sources='$(readlink -m "$rule")'
 EOF
 
-    invoke "$config" 2> "$stderr"
+        invoke "$config" 2> "$stderr"
 
-    if ! compare "$rule (stderr)" "$stderr" < "${rule%.rule}.stderr"; then
-        result=1
-    fi
+        if ! compare "$rule (stderr)" "$stderr" < "${rule%.rule}.stderr"; then
+            result=1
+        fi
+    done
+
+    # Cleanup
+    rm -r "$worktree"
+
+    return "$result"
+}
+
+basedir="$(dirname "$0")"
+opt_shell=sh
+
+while getopts :hs: opt; do
+	case "$opt" in
+		h)
+			echo >&2 "$(basename $0) [-h] [-s <shell>]"
+			echo >&2 '  -h: display help and exit'
+			echo >&2 '  -s <shell>: use specified shell to run tests'
+			exit
+			;;
+
+		s)
+			opt_shell="$OPTARG"
+			;;
+
+		:)
+			echo >&2 "missing argument for option '-$OPTARG'"
+			exit 1
+			;;
+
+		*)
+			echo >&2 "unknown option '-$OPTARG'"
+			exit 1
+			;;
+	esac
 done
 
-# Cleanup
-rm -r "$worktree"
+shift "$((OPTIND - 1))"
 
-exit "$result"
+run "$opt_shell"
+
+exit $?
